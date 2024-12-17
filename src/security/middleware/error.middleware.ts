398112f662c87aa81aa4bfc6133de6c77a00418c
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-17 19:38:40 -0300

    fix(middleware): Corregir la ruta de importación de CustomError en error.middleware

--- src/security/middleware/error.middleware.ts
+++ src/security/middleware/error.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../exeption/custom.error';
+import { CustomError } from '../../exeption/custom.error';
 
 export const errorHandler = (
   err: Error,
