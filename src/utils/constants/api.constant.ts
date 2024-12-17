88e1382964f42269be786a4b98f58bb1165f3c83
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-17 19:31:54 -0300

    feat(api): Añadir constante API_AUTH para la ruta de autenticación

--- src/utils/constants/api.constant.ts
+++ src/utils/constants/api.constant.ts
@@ -6,4 +6,9 @@ export class ApiConstant{
     public static readonly API_BASE:string=`/api/${ApiConstant.API_VERSION}`;
     public static readonly API_USER:string=`${ApiConstant.API_BASE}/user`;
 
+    public static readonly API_AUTH:string=`${ApiConstant.API_BASE}/auth`;
+    public static readonly API_AUTH_LOGIN:string=`${ApiConstant.API_AUTH}/login`;
+    public static readonly API_AUTH_LOGOUT:string=`${ApiConstant.API_AUTH}/logout`;
+    public static readonly API_AUTH_REFRESH:string=`${ApiConstant.API_AUTH}/refresh`;
+
 }
