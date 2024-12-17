c39ca9ad5bb27ce9a98ff6f2d90a12aa2a08729e
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-17 19:33:10 -0300

    feat(user): Permitir la inyección de AppDataSource en UserRepository y añadir método findByEmail

--- src/repository/impements/user.implements.repository.ts
+++ src/repository/impements/user.implements.repository.ts
@@ -2,14 +2,18 @@ import { DataSource, Repository } from "typeorm";
 import { User } from "../../entity/User";
 import { UserInterfaceRepository } from "../interface/user.interface.repository";
 import { NotFoundError } from "../../exeption/not.found.error";
+import { AppDataSource } from "../../configuration/database.config";
 
 export class UserRepository implements UserInterfaceRepository{
     private repository:Repository<User>;
 
-    constructor (datasouce:DataSource){
-        this.repository=datasouce.getRepository(User);
+    constructor (datasouce?: typeof AppDataSource){
+         // Si no se proporciona una fuente de datos, utiliza AppDataSource por defecto
+       const dataSource=datasouce || AppDataSource;
+         // Obtiene el repositorio de la entidad User a partir de la fuente de datos
+       this.repository=dataSource.getRepository(User);
     }
-    async create(user: Partial<User>): Promise<User> {
+    public async create(user: Partial<User>): Promise<User> {
         try{
             const createUser=this.repository.create(user);
             return await this.repository.save(createUser);
@@ -18,7 +22,7 @@ export class UserRepository implements UserInterfaceRepository{
             throw new Error('Error to create user');
         }
     }
-    async findAll(page: number = 1, limit: number =10): Promise<User[]> {
+    public async findAll(page: number = 1, limit: number =10): Promise<User[]> {
         const skip=(page-1)*limit;
         return this.repository.find({
             skip,
@@ -29,10 +33,10 @@ export class UserRepository implements UserInterfaceRepository{
         })
         
     }
-    async findById(id: string): Promise<User | null> {
+    public async findById(id: string): Promise<User | null> {
         return this.repository.findOne({where:{id}});
     }
-    async delete(id: string): Promise<void> {
+    public async delete(id: string): Promise<void> {
         const user=await this.repository.findOne({where:{id}});
         if(!user){
             console.error('User not found');
@@ -41,7 +45,7 @@ export class UserRepository implements UserInterfaceRepository{
         await this.repository.remove(user);
 
     }
-   async update(id: string, userData: Partial<User>): Promise<User> {
+   public async update(id: string, userData: Partial<User>): Promise<User> {
         const user=await this.repository.findOne({where:{id}});
         if(!user){
             console.error('User not found');
@@ -49,6 +53,9 @@ export class UserRepository implements UserInterfaceRepository{
         }
         Object.assign(user, userData);
         return this.repository.save(user);
+    }
+    public async findByEmail(email: string): Promise<User | null> {
+        return this.repository.findOne({where:{email}});
     }
     
 }
