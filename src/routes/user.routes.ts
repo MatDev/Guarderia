022787a8f63fabd81a4e47b0365a5f7be0fd6a84
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-16 22:51:38 -0300

    feat(auth):  Actualizar importación tras cambio de directorio UserService

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { AppDataSource } from '../configuration/database.config';
 import { UserRepository } from '../repository/impements/user.implements.repository';
-import { UserService } from '../service/user.service';
+import { UserService } from '../service/implement/user.service';
 import { UserController } from '../controllers/user.controller';
 
 // Instanciamos las dependencias necesarias
