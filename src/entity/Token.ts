50d8a0119042237a0afe8d5b5a539a2728049a61
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-17 19:30:13 -0300

    feat(token): Añadir entidad Token con propiedades y relaciones a User

---
+++ src/entity/Token.ts
@@ -0,0 +1,29 @@
+import { User } from "./User";
+import { Entity,PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn  } from "typeorm";
+import { TokenType } from "../utils/enum/token.type";
+
+@Entity()
+export class Token {
+  @PrimaryGeneratedColumn('increment')
+  public id: number;
+
+  @Column({ unique: true, name: 'access_token' })
+  public accessToken: string;
+
+  @Column({
+    type: 'enum',
+    enum: TokenType,
+    default: TokenType.BEARER
+  })
+  public tokenType: TokenType;
+
+  @Column({ default: false })
+  public revocado: boolean;
+
+  @Column({ default: false })
+  public expirado: boolean;
+
+  @ManyToOne(() => User, usuario => usuario.tokens, { eager: false })
+    @JoinColumn({ name: 'usuario_id' })
+  public usuario: User;
+}
