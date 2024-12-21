035b718604177e8c38f58db639dbe9e2018958e2
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-21 20:52:17 -0300

    feat(routes): Añadir middleware de autenticación y autorización a las rutas de usuario

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
+import { AuthenticationMiddleware } from '../security/middleware/authentication.middleware';
+import { authorizeRoles } from '../security/middleware/role.middleware';
+import { UserRole } from '../utils/enum/user.role';
 
 // Instanciamos las dependencias necesarias
 
@@ -7,9 +10,9 @@ const userController = new UserController();
 // Configuramos las rutas
 const router = Router();
 
-router.post('/', (req, res) => userController.createUser(req, res));
+router.post('/', AuthenticationMiddleware, authorizeRoles([UserRole.PARVULARIA]), (req, res) => userController.createUser(req, res));
 router.get('/', (req, res) => userController.getAllUsers(req, res));
-router.get('/:id', (req, res) => userController.getUserById(req, res));
+router.get('/:id',AuthenticationMiddleware, authorizeRoles([UserRole.PARVULARIA]), (req, res) => userController.getUserById(req, res));
 router.put('/:id', (req, res) => userController.updateUser(req, res));
 router.delete('/:id', (req, res) => userController.deleteUser(req, res));
 
